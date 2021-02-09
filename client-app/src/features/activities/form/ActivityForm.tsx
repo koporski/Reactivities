9606ef5c5296ed60718a3d3a70911c84f29bf6c9
Konrad <oporski.konrad@gmail.com>
2021-02-09 19:43:17 +0100

    End of section 7

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,23 +1,24 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Form, Segment, Button } from "semantic-ui-react";
 import { IActivity } from "../../../app/modules/activity";
-import {v4 as uuid} from 'uuid';
+import { v4 as uuid } from "uuid";
+import ActivityStore from "../../../app/stores/activityStore";
+import { observer } from "mobx-react-lite";
 
 interface IProps {
-  setEditMode: (editMode: boolean) => void;
-  activity: IActivity | null;
-  createActivity: (activity: IActivity) => void;
-  editActivity: (activity: IActivity) => void;
-  submitting: boolean;
+  activity: IActivity | undefined;
 }
 
-export const ActivityForm: React.FC<IProps> = ({
-  setEditMode,
+const ActivityForm: React.FC<IProps> = ({
   activity: initialFormState,
-  createActivity,
-  editActivity,
-  submitting
 }) => {
+  const activityStore = useContext(ActivityStore);
+  const {
+    createActivity,
+    editActivity,
+    submitting,
+    cancelFormOpen,
+  } = activityStore;
   const initializeForm = () => {
     if (initialFormState) {
       return initialFormState;
@@ -37,18 +38,20 @@ export const ActivityForm: React.FC<IProps> = ({
   const [activity, setActivity] = useState<IActivity>(initializeForm);
 
   const handleSubmit = () => {
-      if (activity.id.length === 0){
-        let newActivity = {
-          ...activity,
-          id: uuid()
-        }
-        createActivity(newActivity);
-      } else {
-        editActivity(activity);
-      }
-  }
+    if (activity.id.length === 0) {
+      let newActivity = {
+        ...activity,
+        id: uuid(),
+      };
+      createActivity(newActivity);
+    } else {
+      editActivity(activity);
+    }
+  };
 
-  const handleInputChange = (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const handleInputChange = (
+    event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
     const { name, value } = event.currentTarget;
     setActivity({ ...activity, [name]: value });
   };
@@ -64,39 +67,45 @@ export const ActivityForm: React.FC<IProps> = ({
         />
         <Form.TextArea
           onChange={handleInputChange}
-          name='description'
+          name="description"
           rows={2}
           placeholder="Description"
           value={activity.description}
         />
         <Form.Input
           onChange={handleInputChange}
-          name='category'
+          name="category"
           placeholder="Category"
           value={activity.category}
         />
         <Form.Input
           onChange={handleInputChange}
-          name='date'
+          name="date"
           type="datetime-local"
           placeholder="Date"
           value={activity.date}
         />
         <Form.Input
           onChange={handleInputChange}
-          name='city'
+          name="city"
           placeholder="City"
           value={activity.city}
         />
         <Form.Input
           onChange={handleInputChange}
-          name='venue'
+          name="venue"
           placeholder="Venue"
           value={activity.venue}
         />
-        <Button loading={submitting} floated="right" positive type="submit" content="Submit" />
         <Button
-          onClick={() => setEditMode(false)}
+          loading={submitting}
+          floated="right"
+          positive
+          type="submit"
+          content="Submit"
+        />
+        <Button
+          onClick={cancelFormOpen}
           floated="right"
           type="button"
           content="Cancel"
@@ -105,3 +114,5 @@ export const ActivityForm: React.FC<IProps> = ({
     </Segment>
   );
 };
+
+export default observer(ActivityForm);
--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -1,23 +1,33 @@
+import { observer } from "mobx-react-lite";
 import React from "react";
+import { useContext } from "react";
 import { Button, Container, Menu } from "semantic-ui-react";
+import ActivityStore from "../../app/stores/activityStore";
 
-interface IProps {
-    openCreateForm: () => void;
-}
-
-export const NavBar: React.FC<IProps> = ({openCreateForm}) => {
-    return (
-        <Menu fixed='top' inverted>
-            <Container>
-                <Menu.Item header>
-                    <img src="/assets/logo.png" alt="logo" style={{marginRight: '10px'}}/>
-                    Reactivities
-                </Menu.Item>
-                <Menu.Item name="Activities" />
-                <Menu.Item>
-                    <Button onClick={openCreateForm} positive content="Create Activity" />
-                </Menu.Item>
-            </Container>
-        </Menu>
-    );
+const NavBar: React.FC = () => {
+  const activityStore = useContext(ActivityStore);
+  return (
+    <Menu fixed="top" inverted>
+      <Container>
+        <Menu.Item header>
+          <img
+            src="/assets/logo.png"
+            alt="logo"
+            style={{ marginRight: "10px" }}
+          />
+          Reactivities
+        </Menu.Item>
+        <Menu.Item name="Activities" />
+        <Menu.Item>
+          <Button
+            onClick={activityStore.openCreateForm}
+            positive
+            content="Create Activity"
+          />
+        </Menu.Item>
+      </Container>
+    </Menu>
+  );
 };
+
+export default observer(NavBar);
