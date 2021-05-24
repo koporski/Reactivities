0cfe9d4989b0f2463ffea162d607801c6d327589
Konrad <oporski.konrad@gmail.com>
2021-05-24 19:00:04 +0200

    End of section 20

--- client-app/src/features/profiles/ProfileContent.tsx
+++ client-app/src/features/profiles/ProfileContent.tsx
@@ -1,22 +1,27 @@
 import React from 'react'
 import {Tab} from 'semantic-ui-react'
+import ProfileFollowings from './ProfileFollowings'
 import ProfilePhotos from './ProfilePhotos'
 
 const panes = [
     {menuItem: 'About', render: () => <Tab.Pane>About content</Tab.Pane>},
     {menuItem: 'Photos', render: () => <ProfilePhotos />},
     {menuItem: 'Activities', render: () => <Tab.Pane>Activities content</Tab.Pane>},
-    {menuItem: 'Followers', render: () => <Tab.Pane>Followers content</Tab.Pane>},
-    {menuItem: 'Following', render: () => <Tab.Pane>Following content</Tab.Pane>},
-]
+    {menuItem: 'Followers', render: () => <ProfileFollowings/>},
+    {menuItem: 'Following', render: () => <ProfileFollowings/>},
+];
 
-const ProfileContent = () => {
+interface IProps {
+    setActiveTab: (activeIndex: any) => void;
+}
+
+const ProfileContent: React.FC<IProps> = ({setActiveTab}) => {
     return (
         <Tab
             menu={{fluid: true, vertical: true}}
             menuPosition='right'
             panes={panes}
-            activeIndex={1}
+            onTabChange={(e, data) => setActiveTab(data.activeIndex)}
         />
     )
 }
